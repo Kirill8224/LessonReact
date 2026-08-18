6100a12bc8949b80e768f09ac8f6300bb4b94c4e
Kirill Tatarenko <Kirilltatarenko97@gmail.com>
2026-08-18 11:15:09 +0300

    fix object

--- src/bbl/useCounter.ts
+++ src/bbl/useCounter.ts
@@ -1,18 +1,18 @@
 type nums= {
   name: string,
-  num: number
+  age: number
 }
 export function reducer(num: nums, acent: string){
   if(acent === 'inc'){
-    return({name: name, num: num.num + 1})
+    return({name: num.name, age: num.age + 1})
   }
   if(acent === 'dec'){
-    return({name: name, num: num.num - 1})
+    return({name: num.name, age: num.age - 1})
   }
   if(acent === 'res'){
-    return({name: name, num: 0})
+    return({name: num.name, age: 0})
   }
   else{
-    return({name: name, num})
+    return({name: num.name, age: num.age})
   }
 }
--- src/ui/count.tsx
+++ src/ui/count.tsx
@@ -2,14 +2,14 @@ import { Card, Button, Typography } from "@mui/material"
 import { reducer } from "../bbl/useCounter"
 import { useReducer } from "react"
 export function Countui(){
-    const nums= {name: 'Kirill', num: 5}
+    const nums= {name: 'Kirill', age: 5}
     const [state, deshier]= useReducer(reducer, nums)
     return(<Card>
         <div style={{display: "flex",  gap: '2px', justifyContent: 'center'}}>
-        <Button variant="contained" onClick={()=>{deshier('inc')}}>{state.num} + 1</Button>
-        <Button variant="contained" onClick={()=>{deshier('dec')}}>{state.num} -1</Button>
+        <Button variant="contained" onClick={()=>{deshier('inc')}}>{state.age} + 1</Button>
+        <Button variant="contained" onClick={()=>{deshier('dec')}}>{state.age} -1</Button>
         <Button variant="contained" onClick={()=>{deshier('res')}}>сброситиь(0)</Button>
         </div>
-        <Typography variant="h1">имя: {state.name}, возраст: {state.num}</Typography>
+        <Typography variant="h1">имя: {state.name}, возраст: {state.age}</Typography>
     </Card>)
 }
