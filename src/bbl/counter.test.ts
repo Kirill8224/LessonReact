fe4eea6497e5ef4b01bc1c166ad4efd0329a15d2
Kirill Tatarenko <Kirilltatarenko97@gmail.com>
2026-08-19 16:36:27 +0300

    update vitest

--- src/bbl/counter.test.ts
+++ src/bbl/counter.test.ts
@@ -9,3 +9,26 @@ test('reducer, typ: res', ()=>{
     const result= reducer({name: 'Kirill', age: 15}, 'res')
     expect(result).toEqual({name: 'Kirill', age: 0})
 })
+test('reducer, typ: dec', ()=>{
+    const result= reducer({name: 'Kirill', age: 15}, 'dec')
+    expect(result).toEqual({name: 'Kirill', age: 14})
+})
+test.each(
+    [
+        [{name: 'Bibi', age: 14}, {name: 'Bibi', age: 15}],
+        [{name: 'Alex', age: 71}, {name: 'Alex', age: 72}],
+        [{name: 'Brbr', age: 25}, {name: 'Brbr', age: 26}],
+        [{name: 'Vlad', age: 54}, {name: 'Vlad', age: 55}],
+    ])('reduce inc', (state, expected)=>{
+        const result= reducer(state, 'inc')
+        expect(result).toEqual(expected)
+    }
+)
+test.each(
+    [
+        [{name:'Kirill', age: 15}, {name: 'Kirill', age: 14}],
+        [{name:'Misha', age: 14}, {name: 'Misha', age: 13}],
+        [{name:'Bibi', age: 71}, {name: 'Bibi', age: 70}],
+    ])('reduce dec', (state, expected)=>{
+        expect(reducer(state, 'dec')).toEqual(expected)
+    })
